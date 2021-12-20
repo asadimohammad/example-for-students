ba6d7d1b1ab57478096c19fd7cd0dd6a496c41c6
Mohammad Asaadi <67.asadi@gmail.com>
2021-12-21 01:53:52 +0330

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu') , 
+      menu = document.querySelector('.menu')
+
+eventListeners()
+function eventListeners(){
+    hamburgerMenu.addEventListener('click' , addToggle)
+}
+
+function addToggle(e){
+    hamburgerMenu.classList.toggle('on')
+}
